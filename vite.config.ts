68680ab94a94648a4088750868675ddc41fb36b5
Alex Bashlii <oleksandr@bstock.com>
2023-09-03 01:06:33 +0300

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-export default defineConfig(({ command }) => {
+// { command }
+export default defineConfig(() => {
   const config = {
     plugins: [react()],
     base: '/',
